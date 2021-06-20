cc1b22e938d05f3a716e086948e5a89ec281c83a
yologolf <surprise.eng000@gmail.com>
2021-06-20 16:31:09 +0300

    breakder - small fix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,7 +1,10 @@
+import { useViewport } from 'components/hooks';
 import Stat from 'components/Stat/Stat'
 import React from 'react'
 import './Statistics.scss';
 const Statistics = () => {
+    const { width } = useViewport();
+
     return (
         <div className="statistics-component">
             <div className="statistics-component__container">
@@ -10,7 +13,7 @@ const Statistics = () => {
                 <Stat name="liquidityPoolBalance" values={["3,379,840.94 (USDT pool)", "2,316,739.83 (ETH pool)"]} />
 
                 <Stat name="openTrades" values={["88,121.72 (USDT pool)", "166,416.22 (ETH pool)"]} />
-                <div className="statistics-component__container--breaker"></div>
+                {width < 1365 && <div className="statistics-component__container--breaker"></div>}
                 <Stat name="goviPrice" value="3.32" />
 
                 <Stat name="feesCollected" value="185,795.07" />
