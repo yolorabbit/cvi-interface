eb590f978d0f454e2a77d22e8392c84917304e38
yologolf <surprise.eng000@gmail.com>
2021-06-30 10:58:09 +0300

    tooltip use memo

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,29 +1,32 @@
 import { useIsDesktop } from 'components/hooks';
-import React from 'react'
+import React, { useMemo } from 'react'
 import './Tooltip.scss';
 
 const Tooltip = ({type = "info", left, mobileLeft, right, maxWidth, minWidth, content, children}) => {
     const isDesktop = useIsDesktop();
-    return (
-        <div className="tooltip">
-            <div className="tooltip-avatar">
-            {type === 'info' && <img src={require('../../images/icons/info.svg').default} alt="info" /> }
-            {type === 'info-warning' && <img src={require('../../images/icons/info-warning.svg').default} alt="warning" /> }
-                {type === 'question' && <img src={require('../../images/icons/question-mark.svg').default} alt="question mark" /> }
-            </div>
-            <span className="tooltip__info" style={{
-                left: !isDesktop ? mobileLeft ?? left : left,
-                right,
-                maxWidth,
-                minWidth
-            }}>
-                <div>
-                    {content}
-                    {children}
+    return useMemo(() => {
+        return (
+            <div className="tooltip">
+                <div className="tooltip-avatar">
+                {type === 'info' && <img src={require('../../images/icons/info.svg').default} alt="info" /> }
+                {type === 'info-warning' && <img src={require('../../images/icons/info-warning.svg').default} alt="warning" /> }
+                    {type === 'question' && <img src={require('../../images/icons/question-mark.svg').default} alt="question mark" /> }
                 </div>
-            </span>
-        </div>
-    )
+                <span className="tooltip__info" style={{
+                    left: !isDesktop ? mobileLeft ?? left : left,
+                    right,
+                    maxWidth,
+                    minWidth
+                }}>
+                    <div>
+                        {content}
+                        {children}
+                    </div>
+                </span>
+            </div>
+        )
+        //eslint-disable-next-line
+    }, [isDesktop]);
 }
 
 export default Tooltip;
