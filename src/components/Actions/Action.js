87030127fec87cc48e4066ba027ac2f9af839a6a
yologolf <surprise.eng000@gmail.com>
2021-07-11 11:52:16 +0300

    buy and deposit connect wallet

--- src/components/Actions/Action.js
+++ src/components/Actions/Action.js
@@ -5,10 +5,19 @@ import stakingConfig from 'config/stakingConfig';
 import PlatformActions from './PlatformActions';
 import StakingActions from './StakingActions';
 import './Action.scss';
+import ConnectWallet from 'components/ConnectWallet';
+import { useActiveWeb3React } from 'components/Hooks/wallet';
+
+const authGuard = {
+    "buy": true,
+    "deposit": true
+}
 
 const Action = () => {
+    const { account } = useActiveWeb3React();
     const { type } = useActionController();
-    
+
+    if(!account && authGuard[type]) return <ConnectWallet type="action auth-guard" />
     if(platformConfig.actionsConfig?.[type]) return <PlatformActions />;
     if(stakingConfig.actionsConfig?.[type]) return <StakingActions />;
     return null;
