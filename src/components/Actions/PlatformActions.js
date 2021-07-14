6d729166c44a67ed6f0ebb842b1a1a1927c9e0cd
yologolf <surprise.eng000@gmail.com>
2021-07-14 14:22:02 +0300

    remove unused code

--- src/components/Actions/PlatformActions.js
+++ src/components/Actions/PlatformActions.js
@@ -1,4 +1,3 @@
-import Button from "components/Elements/Button";
 import platformConfig from "config/platformConfig";
 import { useActionController } from "./ActionController";
 import Buy from "./Buy";
@@ -18,20 +17,6 @@ const PlatformActions = () => {
             case platformConfig.actionsConfig.withdraw.key:
                 return  <Withdraw />
     
-            case platformConfig.actionsConfig.claim.key: {
-                return  <div className="claim-component">
-                    <b>100,587.01164174</b>
-                    <span>&nbsp;GOVI (2,700 GOVI) </span>
-                    <Button 
-                        className="claim-button" 
-                        buttonText="Claim" 
-                        onClick={() => {}} 
-                        disabled={false}
-                        processing={false}
-                    /> 
-                </div>
-            }
-                
             case platformConfig.actionsConfig.deposit.key: {
                 return <Deposit />
             }
