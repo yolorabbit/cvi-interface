67f2214fa0755ec1d410a9266de6613625e6637e
yologolf <surprise.eng000@gmail.com>
2021-07-14 16:00:56 +0300

    platform claim

--- src/components/Actions/PlatformClaim.js
+++ src/components/Actions/PlatformClaim.js
@@ -2,22 +2,29 @@ import Button from 'components/Elements/Button';
 import { useEvents } from 'components/Hooks/useEvents';
 import { useActiveWeb3React } from 'components/Hooks/wallet';
 import { DataState } from 'components/Tables/Elements/Values/DataState';
+import config from 'config/config';
+import { contractsContext } from 'contracts/ContractContext';
 import { useWeb3Api } from 'contracts/useWeb3Api';
 import moment from 'moment';
-import React, { useEffect, useMemo, useRef, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addAlert } from 'store/actions';
+import { gas } from 'utils';
 
 const PlatformClaim = ({token}) => {
+    const dispatch = useDispatch();
     const timerRef = useRef();
+    const contracts = useContext(contractsContext);
     const { library, account } = useActiveWeb3React();
     const accountPayload = useMemo(() => ({account}), [account]);
     const [claimData, updateClaimData] = useWeb3Api("getClaimData", token.key, accountPayload, {errorValue: "0", updateOn: "positions"});
     const { getLatestBlockTimestamp } = useEvents();
     const [processing, setProcessing] = useState(false);
-    
     const { lastEndOfDay, lastEndDate, isClaimAvailable } = claimData?.[0] || {};
 
     const getDiff = async () => {
         try {
+            if(lastEndDate === undefined || lastEndOfDay === undefined) return 0;
             const latestBlockTimestamp = await getLatestBlockTimestamp(library.eth.getBlock) * 1000;
             if(moment.utc(latestBlockTimestamp).isBefore(lastEndOfDay)) return moment.duration(lastEndOfDay.diff(moment.utc(latestBlockTimestamp))).asMilliseconds(); // before midnight, check duration to lastEndOfDay
             if(moment.utc(latestBlockTimestamp).isAfter(lastEndOfDay)) return moment.duration(lastEndDate.diff(moment.utc(latestBlockTimestamp))).asMilliseconds(); // after midnight, check duration to lastEndDate
@@ -43,6 +50,33 @@ const PlatformClaim = ({token}) => {
         }
     }
 
+    const onSubmit = async () => {
+        try {
+            if(!claimData?.length > 0) return;
+           
+            await contracts[token.rel.positionRewards].methods.claimReward().send({ from: account, ...gas })
+
+            dispatch(addAlert({
+                id: 'claim',
+                eventName: "claim positions - success",
+                alertType: config.alerts.types.CONFIRMED,
+                message: "Transaction success!"
+            }));
+
+            await updateClaimData();
+        } catch(error) {
+            console.log(error);
+            dispatch(addAlert({
+                id: 'claim',
+                eventName: "claim positions - failed",
+                alertType: config.alerts.types.FAILED,
+                message: "Transaction failed"
+            }));
+        } finally {
+            setProcessing(false);
+        }
+    }
+
     useEffect(() => {
         if(!library?.eth?.getBlock) return;
         setUpdateClaimAvailableTimer();
@@ -57,21 +91,21 @@ const PlatformClaim = ({token}) => {
 
 
     return (
-        <div className={`claim-component ${!isClaimAvailable ? 'is-not-available' : ''}`}>
+        <div className={`claim-component ${claimData === null ? 'claim-is-loading' : ''} ${!isClaimAvailable ? 'is-not-available' : ''}`}>
             <DataState value={claimData}>
                 {claimData?.map((claim, index) => <React.Fragment key={index}> 
                     <b>{claim.amount}</b>
                     <span>&nbsp;{claim.symbol} ({claim.totalAmount} {claim.symbol}) </span>
-                    <Button 
-                        className="claim-button" 
-                        buttonText="Claim" 
-                        eventOnSubmit
-                        onClick={() => {}} 
-                        processing={processing} 
-                        disabled={processing || !isClaimAvailable}
-                    /> 
                 </React.Fragment>)}
             </DataState>
+
+            <Button 
+                className="claim-button" 
+                buttonText="Claim" 
+                onClick={onSubmit} 
+                processing={processing} 
+                disabled={processing || !isClaimAvailable}
+            /> 
         </div>
     )
 }
