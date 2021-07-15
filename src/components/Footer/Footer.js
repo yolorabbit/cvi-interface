8322f5ea55aa143bab33d160534a989367180f44
yologolf <surprise.eng000@gmail.com>
2021-07-15 13:51:43 +0300

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,14 +6,14 @@ const Footer = () => {
     return (
         <div className="footer-component">
             <div className="footer-component__container">
-                <div className="footer-component__container--left">
-                    <span>© 2021 all rights reserved to COTI</span>
-                </div>
-
                 <div className="footer-component__container--right">
                     {config.socialLinks.map(({iconName, to}) => <a key={iconName} href={to} rel="noopener noreferrer" target="_blank">
                         <img src={require(`../../images/social/${iconName}.svg`).default} alt={iconName} />
                     </a>)}
+                </div>
+
+                <div className="footer-component__container--left">
+                    <span>© 2021 all rights reserved to COTI</span>
                 </div>
             </div>
         </div>
