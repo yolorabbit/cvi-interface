e37b32d3ce2e0611bcbba91cbc68f359f4dbfd83
yologolf <surprise.eng000@gmail.com>
2021-07-15 14:06:30 +0300

    remove - from lp

--- src/components/Tables/Elements/Values/Coin.js
+++ src/components/Tables/Elements/Values/Coin.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Coin = ({token, showName}) => {
   if(!token) return null;
+
+  const tokenName = token.split('-').length > 1 ? token.replace(/-([^-]*)$/, ' $1') : token;
+ 
   return <> 
     <img className="coin-component--img" src={require(`images/coins/${token}.svg`).default} alt={token} />
-    {showName && <span className="coin-component--text">{token.toUpperCase()}</span>}
+    {showName && <span className="coin-component--text">{tokenName.toUpperCase()}</span>}
   </>
 }
 
