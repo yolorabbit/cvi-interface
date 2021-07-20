baec052f2e0cb767b85cdea838c4d42228346e50
yologolf <surprise.eng000@gmail.com>
2021-07-20 18:27:13 +0300

    update help center

--- src/components/pages/HelpCenter/Guides/Guides.js
+++ src/components/pages/HelpCenter/Guides/Guides.js
@@ -1,41 +1,47 @@
 import React, { useMemo } from 'react'
+import { track } from 'shared/analytics';
 
 const Guides = () => {
   return useMemo(() => {
     const onWhitepaperClick = () => {
-        // track('Whitepaper');
+        track('Whitepaper');
         window.open('/files/cvi-white-paper.pdf');
     }
 
     const onUsdtAnnouncementClick = () => {
-        // track('USDT Audit');
+        track('USDT Audit');
         window.open('/files/usdt-audit.pdf');
     }
 
     const onEthAnnouncementClick = () => {
-        // track('ETH Audit');
-        window.open('/files/eth-audit.pdf');
+      track('ETH Audit');
+      window.open('/files/eth-audit.pdf');
+    }
+
+    const onPeckShieldAnnouncementClick = () => {
+      track('Pech shield audit');
+      window.open('/files/peckshield_audit_report_coti.pdf');
     }
 
     return (
       <div className="guides-component">
-        <h2>Guides</h2>
+        {/* <h2>Guides</h2>
   
         <div className="guides-component__guide">
-        <img src={require('../../../../images/icons/guide.svg').default} alt="guide" />
+          <img src={require('../../../../images/icons/guide.svg').default} alt="guide" />
           <div className="guides-component__guide--body">
             <h3>Crypto Volatility Index Manual</h3>
             <p>We’ve prepared this tutorial to help you better understand the CVI features and trading options</p>
           </div>
-        </div>
+        </div> */}
   
         <h2>Documents & Audits</h2>
 
         <div className="documents-component">
           <Document title="Whitepaper" onClick={onWhitepaperClick} />
-          <Document title="Volatility token" onClick={() => {}}  />
-          <Document title="USDT Audit" date="January 6th, 2021" onClick={onUsdtAnnouncementClick}  />
-          <Document title="ETH Audit" date="March 29th, 2021" onClick={onEthAnnouncementClick}  />
+          <Document title="Volatility token" onClick={onPeckShieldAnnouncementClick}  />
+          <Document title="CERTIK Audit" date="January 6th, 2021" onClick={onUsdtAnnouncementClick}  />
+          <Document title="CERTIK Audit" date="March 29th, 2021" onClick={onEthAnnouncementClick}  />
         </div>
       </div>
     )
