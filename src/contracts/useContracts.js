4655023254c67e918d1f8e4096747f83acb3296a
yologolf <surprise.eng000@gmail.com>
2021-07-30 18:06:42 +0300

    fix set provider

--- src/contracts/useContracts.js
+++ src/contracts/useContracts.js
@@ -1,4 +1,5 @@
 import { useWeb3React } from "@web3-react/core";
+import { useActiveWeb3React } from "components/Hooks/wallet";
 import config from "config/config";
 import { useEffect, useMemo, useState } from "react"
 import { useSelector } from "react-redux";
@@ -7,15 +8,17 @@ import Contract from "web3-eth-contract";
 export const useContracts = () => {
     const { selectedNetwork } = useSelector(({app}) => app);
     const { library } = useWeb3React(config.web3ProviderId);
+    const { library: web3 } = useActiveWeb3React();
     const [contracts, setContracts] = useState(null);
 
     useEffect(() => {
         const loadContracts = () => {
-            if(!library?.currentProvider) return;
+            if(!library?.currentProvider || !web3?.currentProvider) return;
             const contractsJSON = require(`./files/${process.env.REACT_APP_ENVIRONMENT}/Contracts_${selectedNetwork}.json`);
             const contractsKeys = Object.keys(contractsJSON);
             let contractsObject = {};
-            Contract.setProvider(library?.currentProvider);
+            const provider = selectedNetwork === "Matic" ? library?.currentProvider : web3?.currentProvider
+            Contract.setProvider(provider);
 
             contractsKeys.forEach((key) => {
                 contractsObject[key] = new Contract(contractsJSON[key].abi, contractsJSON[key].address);
@@ -26,7 +29,8 @@ export const useContracts = () => {
 
         loadContracts();
         // eslint-disable-next-line
-    }, [library]);
+    }, [library, web3]);
+
     
     return useMemo(() => {
         return contracts;
