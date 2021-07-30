317fb0f94b4301d93dc580871b01f1b7ff38a5c4
yologolf <surprise.eng000@gmail.com>
2021-07-30 17:00:13 +0300

    staking approval current provider

--- src/components/Hooks/useApproveToken.js
+++ src/components/Hooks/useApproveToken.js
@@ -1,23 +1,36 @@
 import config from "config/config";
 import { contractsContext } from "contracts/ContractContext";
 import { useCallback, useContext } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addAlert } from "store/actions";
 import { maxUint256, toBN, toBNAmount } from "utils";
+import Contract from "web3-eth-contract";
 import { useActiveWeb3React } from "./wallet";
 
 const useApproveToken = () => {
-    const { account } = useActiveWeb3React();
+    const { account, library } = useActiveWeb3React();
     const contracts = useContext(contractsContext);
     const dispatch = useDispatch()
-
+    const { selectedNetwork } = useSelector(({app})=>app);
+    
     const allowance = useCallback(async (token, address) => {
-        return await contracts[token.key === "govi" ? "GOVI" : token.rel.platform ?? token.rel.token].methods.allowance(account, address).call();
+        const _contract = getContract(token.key === "govi" ? "GOVI" : token.rel.platform ?? token.rel.token)
+        return await _contract.methods.allowance(account, address).call();
     },[account, contracts]);
     
     const approve = useCallback(async (token, address) => {
-        return await contracts[token.key === "govi" ? "GOVI" : token.rel.platform ?? token.rel.token].methods.approve(address, maxUint256).send({from: account});
+        const _contract = getContract(token.key === "govi" ? "GOVI" : token.rel.platform ?? token.rel.token)
+        return await _contract.methods.approve(address, maxUint256).send({from: account});
     },[account, contracts]);
+
+    const getContract = (contractKey) => {
+        const contractsJSON = require(`../../contracts/files/${process.env.REACT_APP_ENVIRONMENT}/Contracts_${selectedNetwork}.json`);
+        const { abi, address } = contractsJSON[contractKey];
+        const _contract = new Contract(abi, address);
+        _contract.setProvider(library?.currentProvider);
+        return _contract
+    }
+    
 
     const approvalValidation = async (token, amount) => {
         const { _address: address } = contracts[token.rel.stakingRewards];
