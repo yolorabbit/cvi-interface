bd8ce4769386fdfa75a2c26dad62abfdb3d09d8c
yologolf <surprise.eng000@gmail.com>
2021-08-04 13:48:28 +0300

    external link title style

--- src/components/Tables/Elements/Values/Pairs.js
+++ src/components/Tables/Elements/Values/Pairs.js
@@ -14,7 +14,7 @@ const Pairs = ({leftToken, rightToken, label, protocol, showImage, hideNames, po
             <span>{label ?? `${leftToken?.toUpperCase()}-${rightToken?.toUpperCase()} LP`}</span>
             <span>{protocol}</span>
           </div>}
-          {poolLink && <a className="external-link" href={poolLink} target="_blank" rel="noopener noreferrer">
+          {poolLink && <a className="external-link" title={`View on ${protocol}`} href={poolLink} target="_blank" rel="noopener noreferrer">
             <img src={require('images/icons/pop.svg').default} alt="link"/>
           </a> }
       </div> }
