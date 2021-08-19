fadca94513eeed47d3bd4ebdf5e2deeffefa69d8
yologolf <surprise.eng000@gmail.com>
2021-08-19 17:43:32 +0300

    fix daily reward

--- src/components/Hooks/useEvents.js
+++ src/components/Hooks/useEvents.js
@@ -118,16 +118,13 @@ export const useEvents = () => {
     }
   
     async function getTransferEvents(staking, token, days, tokenName) {
-        if(chainName === chainNames.Matic) {
-            const _events = await TheGraph[`collectedFees${tokenName === "USDC" ? "USDC" : ""}`](Math.floor(moment.utc().add(-days, 'days').valueOf() / 1000));
-            return _events.collectedFees
-        }
+        // if(chainName === chainNames.Matic) {
+        //     const _events = await TheGraph[`collectedFees${tokenName === "USDC" ? "USDC" : ""}`](Math.floor(moment.utc().add(-days, 'days').valueOf() / 1000));
+        //     return _events.collectedFees
+        // }
         let isETH = false;
         if (token._address.toLowerCase() === "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2") isETH = true;
-        let options = {};
-        const latestBlockNumber = await (await getBlock("latest")).number;
-        const stepSize = latestBlockNumber - bottomBlockByNetwork[chainName]
-        options = { days, stepSize: (parseInt(stepSize / DEFAULT_STEPS) + 1000), steps: DEFAULT_STEPS };
+        const options = { days, stepSize: 2000, steps: Number.MAX_SAFE_INTEGER };
         const filter = { [isETH ? "dst" : "to"]: staking._address };
         const eventsData = [{ contract: token, events: { Transfer: [filter] } }];
         return await getEvents(eventsData, options, getBlock);
