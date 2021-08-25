2c000a535237981d558a54d675b5822a3d822075
yologolf <surprise.eng000@gmail.com>
2021-08-25 00:08:42 -0700

    refactored components and options

--- src/components/pages/CVIIndex/CVIIndex.js
+++ src/components/pages/CVIIndex/CVIIndex.js
@@ -1,27 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import moment from 'moment';
-//TODO: further customizations available at https://www.npmjs.com/package/@jtdaugh/react-lightweight-charts
 import Chart from 'kaktana-react-lightweight-charts';
 import Column from 'components/Layout/Column';
 import Container from 'components/Layout/Container';
 import Layout from 'components/Layout/Layout';
 import Row from 'components/Layout/Row';
+import { Loading, Error } from './HelperComponents';
 import config from '../../../config/config';
-//TODO: all parameters and customizations available at https://github.com/tradingview/lightweight-charts
-import options from './chartOptions.json';
+import options from './chartOptions.js';
 import historicalData from '../../CviIndexGraph/historicalData.json';
 //TODO: create a dedicated css data - right now taken from staking component
 import '../Staking/Staking.scss';
 
-//TODO: better styling of error component
-const Error = ({message}) => {
-  <Container title="Error occured" style={{background: 'red', text: 'white'}}>
-    {message}
-  </Container>
-};
-
-//TODO: better styling of loading component
-const Loading = () => <Container>Loading...</Container>;
 
 const CVIIndex = () => {
   const [data, setData] = useState(undefined);
@@ -70,30 +60,27 @@ const CVIIndex = () => {
       <Column>
         <Row>
           <Container title="CVI Index">
-            <>
-              {
-                error ?
-                  <Error message={error}/>
-                :
-                  <>
-                    {
-                      data ?
-                        <div style={{padding: '5px'}}>
-                          <Chart
-                            options={options}
-                            lineSeries={data}
-                            darkTheme
-                            height={350}
-                            autoWidth 
-                          />
-                        </div>  
-                      :
-                        <Loading />
-                    }
-                  </>
-              }
-            </>
-     
+          {
+            error ?
+              <Error message={error}/>
+            :
+              <>
+                {
+                  data ?
+                    <div style={{padding: '5px'}}>
+                      <Chart
+                        options={options}
+                        lineSeries={data}
+                        darkTheme
+                        height={350}
+                        autoWidth 
+                      />
+                    </div>  
+                  :
+                    <Loading />
+                }
+              </>
+          }
           </Container>
         </Row>
       </Column>
---
+++ src/components/pages/CVIIndex/chartOptions.js
@@ -0,0 +1,11 @@
+
+//TODO: further customizations available at https://www.npmjs.com/package/@jtdaugh/react-lightweight-charts
+//TODO: all parameters and customizations available at https://github.com/tradingview/lightweight-charts
+export default {
+  color: '#f48fb1',
+  lineStyle: 0,
+  lineWidth: 1,
+  crosshairMarkerVisible: true,
+  crosshairMarkerRadius: 6,
+  lineType: 1
+};
