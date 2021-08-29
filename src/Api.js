262652838cca7361edbead7bbf2ccd0172af4a9a
yologolf <surprise.eng000@gmail.com>
2021-08-29 19:45:23 +0300

    use apiV2

--- src/Api.js
+++ src/Api.js
@@ -1,16 +1,12 @@
 import axios from 'axios';
 
-const { REACT_APP_HOST: baseURL, REACT_APP_HOST_V2: baseUrlV2 } = process.env;
+const { REACT_APP_HOST_V2: baseUrlV2 } = process.env;
 
 const headers = {
    'Content-Type': 'application/json',
    'cache-control': 'no-cache',
 };
 
-const axs = axios.create({
-   baseURL,
-   headers,
-});
 
 const axsV2 = axios.create({
    baseURL: baseUrlV2,
@@ -19,8 +15,8 @@ const axsV2 = axios.create({
 
 
 const Api = {
-   GET_INDEX_HISTORY: (chainName) => axs.get(`/history?chain=${chainName}`),
-   GET_INDEX_LATEST: (chainName) => axs.get(`/latest?chain=${chainName}`),
+   GET_INDEX_HISTORY: (chainName) => axsV2.get(`/history?chain=${chainName}`),
+   GET_INDEX_LATEST: (chainName) => axsV2.get(`/latest?chain=${chainName}`),
    GET_FEES_COLLECTED: (queryParams = "") => axsV2.get('/fees' + queryParams)
 };
 
