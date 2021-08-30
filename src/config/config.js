2760464af67d739a9c4ecf8e0e579ee9fa468899
yologolf <surprise.eng000@gmail.com>
2021-08-30 14:43:21 +0300

    enable analytic link

--- src/config/config.js
+++ src/config/config.js
@@ -30,11 +30,11 @@ const config = {
       label: "Staking",
       path: '/staking'
     },
-    // analytics: {
-    //   label: "Analytics",
-    //   path: 'https://dune.xyz/govidev/CVI',
-    //   external: true
-    // },
+    analytics: {
+      label: "Analytics",
+      path: 'https://dune.xyz/govidev/CVI',
+      external: true
+    },
     "help-center": {
       label: "Help center",
       path: '/help'
