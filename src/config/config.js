14c957a25c33ce10fe310d8188e1e7eb2ad5199a
yologolf <surprise.eng000@gmail.com>
2021-10-11 17:48:34 +0300

    Revert "reduce /index route"
    This reverts commit 77d869e7ef69b5aea4b254d42064b6d2a2843bc7.

--- src/config/config.js
+++ src/config/config.js
@@ -50,6 +50,11 @@ const config = {
       path: 'https://docs.cvi.finance',
       external: true
     },
+    cviindex: {
+      label: "CVI Index",
+      path: '/index',
+      dataUrl: 'https://api-v2.cvi.finance/history'
+    }
   },
   socialLinks: [
     { iconName: 'github', to: 'https://github.com/coti-io/cvi-contracts' },
