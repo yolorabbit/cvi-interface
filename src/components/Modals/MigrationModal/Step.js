c7f437f0a7b4f73dfc62be4618563746ccaecd85
yologolf <surprise.eng000@gmail.com>
2021-10-25 18:02:32 +0300

    Update Step Compononet

--- src/components/Modals/MigrationModal/Step.js
+++ src/components/Modals/MigrationModal/Step.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Step.scss";
 
 export const Step = ({ stepData, stepNumber, isActive, isConfirmed }) => {
-console.log(stepNumber, " isActive ", isActive)
+
   return (
     <div className={`step-wrapper${isActive ? ' active' : ''}`}>
       <div className="circle">
-        {stepNumber+1}
+        {stepNumber}
         {isConfirmed && <img className="step-confirmed-icon" src={require('../../../images/icons/confirmed-icon.svg').default} alt="confirmed" />}
         </div>
       <span className="step-title">{stepData.stepTitle}</span>
