bbd61a4d8ceff39d0e28f27b1c730f355a4fdd3c
yologolf <surprise.eng000@gmail.com>
2021-11-25 12:06:22 +0200

    Update Dropdown Validation

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import "./Dropdown.scss";
 
-const Dropdown = () => {
-  const [dropdownValue, setDropdownValue] = useState(3);
+const Dropdown = ( {  label, type, initialValue, dropdownOptions }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const dropdownLabel = "hours";
-  const dropdownValues = [1, 2, 3];
+  const [dropdownValue, setDropdownValue] = useState(initialValue);
 
   const dropdownToggle = () => {
     setIsOpen(!isOpen);
@@ -16,38 +14,42 @@ const Dropdown = () => {
     setIsOpen(false);
   };
 
+  const valueChange = ({target: { value }}) => {
+    if (value === '') return setDropdownValue(''); // Clear input
+    if (type === "number" && (isNaN(Number(value)) || !dropdownOptions.includes(Number(value)))) return; // Only Numbers, Only if included in options
+    setDropdownValue(value);
+  };
+
   return (
-    <div className={`dropdown-component${isOpen ? " open" : ""}`}>
+    <div className={`dropdown-component${isOpen ? ' open' : ''}${(dropdownValue === '') ? ' error' : ''}`}>
       <div className="dropdown-group">
         <div className="input-group">
-          <input type="text"
-            placeholder={dropdownValue}
+          <input type="text" placeholder={dropdownValue}
+            className="dropdown-input"
             name="dropdown-input"
             autoComplete="off"
-            className="dropdown-input"
-            value={dropdownValue} />
+            value={dropdownValue}
+            onChange={valueChange} />
           <img className="open-icon"
             src={require("../../images/icons/dropdown-chevron-orange.svg").default}
             alt="chevron"
             onClick={dropdownToggle} />
         </div>
-        <div className="dropdown-label">{dropdownLabel}</div>
+        <div className="dropdown-label">{label}</div>
       </div>
       {isOpen && (
         <ul className="dropdown-component-open">
-          {dropdownValues.map((value, index) => {
+          {dropdownOptions.map((value, index) => {
             return (
               <li key={index}
-                className={`dropdown-option${
-                value === dropdownValue ? " selected" : ""}`}
-                onClick={() => dropdownSelection(value)}>
+                className={`dropdown-option${value === dropdownValue ? " selected" : "" }`}
+                onClick={() => dropdownSelection(value)} >
                 {value}
               </li>
             );
           })}
         </ul>
       )}
-      ;
     </div>
   );
 };
