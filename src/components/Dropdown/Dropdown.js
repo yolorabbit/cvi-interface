67c91e6d6ed5ebdfe24296cfbe61d378c0967f98
yologolf <surprise.eng000@gmail.com>
2021-11-25 16:39:48 +0200

    TimeToFullfill, Dropdown absolute

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,57 +1,74 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import "./Dropdown.scss";
 
-const Dropdown = ( {  label, type, initialValue, dropdownOptions }) => {
+const Dropdown = ({ label, type, initialValue, dropdownOptions, prefix }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [dropdownValue, setDropdownValue] = useState(initialValue);
 
-  const dropdownToggle = () => {
+  const dropdownToggle = useCallback(() => {
     setIsOpen(!isOpen);
-  };
+  }, [isOpen]);
 
-  const dropdownSelection = (value) => {
-    setDropdownValue(value);
-    setIsOpen(false);
-  };
-
-  const valueChange = ({target: { value }}) => {
-    if (value === '') return setDropdownValue(''); // Clear input
-    if (type === "number" && (isNaN(Number(value)) || !dropdownOptions.includes(Number(value)))) return; // Only Numbers, Only if included in options
-    setDropdownValue(value);
-  };
-
-  return (
-    <div className={`dropdown-component${isOpen ? ' open' : ''}${(dropdownValue === '') ? ' error' : ''}`}>
-      <div className="dropdown-group">
-        <div className="input-group">
-          <input type="text" placeholder={dropdownValue}
-            className="dropdown-input"
-            name="dropdown-input"
-            autoComplete="off"
-            value={dropdownValue}
-            onChange={valueChange} />
-          <img className="open-icon"
-            src={require("../../images/icons/dropdown-chevron-orange.svg").default}
-            alt="chevron"
-            onClick={dropdownToggle} />
-        </div>
-        <div className="dropdown-label">{label}</div>
-      </div>
-      {isOpen && (
-        <ul className="dropdown-component-open">
-          {dropdownOptions.map((value, index) => {
-            return (
-              <li key={index}
-                className={`dropdown-option${value === dropdownValue ? " selected" : "" }`}
-                onClick={() => dropdownSelection(value)} >
-                {value}
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </div>
+  const valueChange = useCallback(
+    ({ target: { value } }) => {
+      if (value === "") return setDropdownValue(""); // Clear input
+      if (
+        type === "number" &&
+        (isNaN(Number(value)) || !dropdownOptions.includes(Number(value)))
+      )
+        return; // Only Numbers, Only if included in options
+      setDropdownValue(value);
+    },
+    [dropdownOptions, type]
   );
+
+  return useMemo(() => {
+    const dropdownSelection = (value) => {
+      setDropdownValue(value);
+      setIsOpen(false);
+    };
+
+    return (
+      <div
+        className={`dropdown-component${isOpen ? " open" : ""}${dropdownValue === "" ? " error" : ""}`}>
+        <div className="dropdown-group">
+          <div className="input-group">
+            <input
+              type="text"
+              className="dropdown-input"
+              name="dropdown-input"
+              value={dropdownValue}
+              onChange={valueChange}
+            />
+
+            <img
+              className="open-icon"
+              src={require("../../images/icons/dropdown-chevron-orange.svg").default}
+              alt="chevron"
+              onClick={dropdownToggle}
+            />
+          </div>
+
+          <div className="dropdown-label">{label}</div>
+        </div>
+        {isOpen && (
+          <ul className="dropdown-component-open">
+            {dropdownOptions.map((value, index) => {
+              return (
+                <li
+                  key={index}
+                  className={`dropdown-option${value === dropdownValue ? " selected" : ""}`}
+                  onClick={() => dropdownSelection(value)}
+                >
+                  {value}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    );
+  }, [ label, dropdownOptions, dropdownValue, isOpen, dropdownToggle, valueChange ]);
 };
 
 export default Dropdown;
--- src/components/pages/Arbitrage/Arbitrage.js
+++ src/components/pages/Arbitrage/Arbitrage.js
@@ -1,34 +1,34 @@
 import React, { useState, useMemo } from "react";
-import { appViewContext } from 'components/Context';
+import { appViewContext } from "components/Context";
 import SubNavbar from "components/SubNavbar";
-import Layout from 'components/Layout/Layout';
-import Row from 'components/Layout/Row';
+import Layout from "components/Layout/Layout";
+import Row from "components/Layout/Row";
 import arbitrageConfig from "config/arbitrageConfig";
 import Container from "components/Layout/Container";
-import ArbitrageTables from './ArbitrageTables/ArbitrageTables';
+import ArbitrageTables from "./ArbitrageTables/ArbitrageTables";
 import "./Arbitrage.scss";
 import MainTabsContainer from "components/MainTabsContainer";
 import Form from "./Form";
 import config from "config/config";
+import TimeToFullfill from "./TimeToFullfill";
 
 const Arbitrage = () => {
   const [activeView, setActiveView] = useState();
-
+  
   return useMemo(() => {
     return (
-        <div className="arbitrage-component">
-            <SubNavbar 
-                tabs={Object.keys(arbitrageConfig.tabs["sub-navbar"])} 
-                activeView={activeView} 
-                setActiveView={setActiveView} />
-            
-            <appViewContext.Provider value={{activeView}}>
-                <Layout>
-                    <Row className="statistics-row-component">
-                        <Container>
+      <div className="arbitrage-component">
+        <SubNavbar
+          tabs={Object.keys(arbitrageConfig.tabs["sub-navbar"])}
+          activeView={activeView}
+          setActiveView={setActiveView}
+        />
 
-                        </Container>
-                    </Row>
+        <appViewContext.Provider value={{ activeView }}>
+          <Layout>
+            <Row className="statistics-row-component">
+              <Container></Container>
+            </Row>
 
                     <Row flex="100%">
                         <MainTabsContainer path={config.routes.arbitrage.path}>
