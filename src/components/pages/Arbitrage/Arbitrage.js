8aa4ac4b32b63e160b75e7a9e29c44639a190cf2
yologolf <surprise.eng000@gmail.com>
2021-11-25 12:06:22 +0200

    Update Dropdown Validation

--- src/components/pages/Arbitrage/Arbitrage.js
+++ src/components/pages/Arbitrage/Arbitrage.js
@@ -7,6 +7,7 @@ import arbitrageConfig from "config/arbitrageConfig";
 import Container from "components/Layout/Container";
 import ArbitrageTables from './ArbitrageTables/ArbitrageTables';
 import "./Arbitrage.scss";
+import Dropdown from "components/Dropdown";
 
 const Arbitrage = () => {
   const [activeView, setActiveView] = useState();
@@ -17,8 +18,7 @@ const Arbitrage = () => {
             <SubNavbar 
                 tabs={Object.keys(arbitrageConfig.tabs["sub-navbar"])} 
                 activeView={activeView} 
-                setActiveView={setActiveView} 
-            />
+                setActiveView={setActiveView} />
             
             <appViewContext.Provider value={{activeView}}>
                 <Layout>
@@ -29,11 +29,14 @@ const Arbitrage = () => {
                     </Row>
 
                     <Row flex="100%">
-                        <Container>
-                            
-                        </Container>
+                        <div>
+                            <h2>Time to fulfillment</h2>
+                            <span>(between 1 to 3 hours)</span>
+                            <Dropdown label="hours" dropdownOptions={[1, 2, 3]} initialValue={3} type="number" />
+                            <Dropdown label="minutes" dropdownOptions={Array.from(Array(60).keys())} initialValue={0} type="number" />
+                            <Dropdown label="yovel" dropdownOptions={["option 1", "option 2", "option 3"]} initialValue={"hello"} type="text" />
+                        </div>
                     </Row>
-
                     <Row>
                         <ArbitrageTables />
                     </Row>
