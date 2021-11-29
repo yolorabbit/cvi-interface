de4fb47f50d6fc8e4578f3a7a98eb5830a9f339d
yologolf <surprise.eng000@gmail.com>
2021-11-29 14:57:36 +0200

    fix history table row

--- src/components/Tables/Elements/Rows/HistoryRow.js
+++ src/components/Tables/Elements/Rows/HistoryRow.js
@@ -4,30 +4,31 @@ import RowItem from './RowItem';
 import { appViewContext } from "components/Context";
 import platformConfig from "config/platformConfig";
 import { Value } from "../Values";
-import { omit } from "lodash";
+import { isEmpty, omit } from "lodash";
 import arbitrageConfig from "config/arbitrageConfig";
 
 const HistoryRow = ({rowData, isHeader}) => {
     const isTablet = useIsTablet();
     const { activeView } = useContext(appViewContext); 
-    const headers = useMemo(() =>  Object.values(arbitrageConfig.tables?.[activeView]?.history.headers) || 
-        Object.values(platformConfig.headers?.[activeView]?.History), [activeView]);
-    
+    const arbitrageHeaders = arbitrageConfig.tables?.[activeView]?.history?.headers;
+    const platformHeaders = platformConfig.headers?.[activeView]?.History;
+  
+    const headers = useMemo(() => {
+        if(!isEmpty(arbitrageHeaders)) return Object.values(arbitrageHeaders);
+        if(!isEmpty(platformHeaders)) return platformHeaders;
+        return null;
+    }, [arbitrageHeaders, platformHeaders]);
+
   
     const RowData = useMemo(() => {
         if(!rowData || !activeView || !headers) return null;
         const historyData = omit(rowData, ['transactionHash', 'timestamp']);
 
-        return (
-            <> 
-                {Object.values(historyData)?.map((value, index) => <RowItem 
-                    key={index}
-                    header={headers[index]} 
-                    content={<Value subText={value} />} 
-                />)}
-            </>
-            //eslint-disable-next-line
-        )
+        return Object.values(historyData)?.map((value, index) => <RowItem 
+            key={index}
+            header={headers[index]} 
+            content={<Value subText={value} />} 
+        />)
     }, [rowData, activeView, headers]);
 
     if(isHeader) {
