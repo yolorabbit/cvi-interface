6f24bcf2c728fcda96ea00dc9bc9929829f3c47f
yologolf <surprise.eng000@gmail.com>
2021-11-29 22:15:09 +0200

    arbitrage page stats data

--- src/components/Tables/Elements/Values/Value.js
+++ src/components/Tables/Elements/Values/Value.js
@@ -3,26 +3,28 @@ import { DataState } from './DataState';
 import { commaFormatted } from 'utils';
 import Coin from './Coin';
 
-const Value = ({className, text, subText, prefix, bottomText, protocol, format, progressBarPercents, coin, showData = true}) => {
-
+const Value = ({className, text, header, subText, prefix, bottomText, protocol, format, progressBarPercents, coin, showData = true}) => {
     return useMemo(() => {
         return (
             <div className={`value-component ${className ?? ''}`}>
                 {!showData ? <span>-</span> :
-                    <DataState value={text === null ? null : text ?? subText}>
-                        {coin && <Coin token={coin} />}
-                        {prefix && <span>{prefix}</span>}
-                        {text && <b>{progressBarPercents ? 
-                            <span style={{width: `${progressBarPercents}%`}}>{commaFormatted(format) ?? text}</span> : 
-                            commaFormatted(format) ?? text}
-                        </b>}
-                        <span>{(text && subText) ? <>&nbsp;{subText}</> : subText}</span>
-                        {bottomText && <div>{bottomText} {protocol && protocol !== "platform" && <span className="value-component__protocol">{`(${protocol})`}</span>}</div>}
-                    </DataState>
+                    <>
+                        {header && <h5>{header}</h5>} 
+                        <DataState value={text === null ? null : text ?? subText}>
+                            {coin && <Coin token={coin} />}
+                            {prefix && <span>{prefix}</span>}
+                            {text && <b>{progressBarPercents ? 
+                                <span style={{width: `${progressBarPercents}%`}}>{commaFormatted(format) ?? text}</span> : 
+                                commaFormatted(format) ?? text}
+                            </b>}
+                            <span>{(text && subText) ? <>&nbsp;{subText}</> : subText}</span>
+                            {bottomText && <div>{bottomText} {protocol && protocol !== "platform" && <span className="value-component__protocol">{`(${protocol})`}</span>}</div>}
+                        </DataState>
+                    </>
                 }
             </div>
         )
-    }, [className, showData, text, subText, prefix, coin, progressBarPercents, format, bottomText, protocol]);
+    }, [className, showData, header, text, subText, coin, prefix, progressBarPercents, format, bottomText, protocol]);
 }
 
 export default Value;
--- src/components/pages/Arbitrage/Statistics/Statistics.js
+++ src/components/pages/Arbitrage/Statistics/Statistics.js
@@ -1,40 +1,53 @@
+import { appViewContext } from 'components/Context';
+import { useActiveToken } from 'components/Hooks';
 import Container from 'components/Layout/Container';
 import IndexValue from 'components/pages/Platform/IndexStats/IndexValue';
 import { Value } from 'components/Tables/Elements/Values';
 import arbitrageConfig from 'config/arbitrageConfig';
-import React, { useMemo } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { useSelector } from 'react-redux';
+import { customFixed } from 'utils';
 import './Statistics.scss';
 
 const Statistics = () => {
-    const { selectedNetwork } = useSelector(({app}) => app);
+    const { selectedNetwork } = useSelector(({ app }) => app);
+    const { w3 } = useContext(appViewContext);
+    const activeToken = useActiveToken();
 
     return useMemo(() => {
-        if(!selectedNetwork) return null;
-        const loadActiveVol = Object.values(arbitrageConfig.tokens[selectedNetwork]).map(({oracleId}) => oracleId)[0];
+        if (!selectedNetwork || !activeToken) return null;
+
+        const loadActiveVol = Object.values(arbitrageConfig.tokens[selectedNetwork]).map(({ oracleId }) => oracleId)[0];
+        const tokenContract = w3?.tokens[activeToken.rel.contractKey];
+        const statisticsDetails = {
+            tokenPrice: tokenContract?.price ?? null,
+            intrinsicValue: tokenContract?.intrinsicPrice ?? null,
+        }
+        const { tokenPrice, intrinsicValue } = statisticsDetails;
+
 
         return (
             <Container className="arbitrage-statistics-component">
-                <Value 
-                    subText="Index" 
-                    bottomText={<IndexValue 
+                <Value
+                    subText="Index"
+                    bottomText={<IndexValue
                         type="arbitrage-statistics"
-                        activeIndex={loadActiveVol} 
+                        activeIndex={loadActiveVol}
                     />}
                 />
 
-                <Value 
-                    subText="Platform price"
-                    bottomText="1000 USDC" 
+                <Value
+                    header="Platform price"
+                    text={intrinsicValue ? customFixed(intrinsicValue, activeToken.fixedDecimals) : null}
                 />
 
-                <Value 
-                    subText="Uniswap price"
-                    bottomText="1000 USDC" 
+                <Value
+                    header="Uniswap price"
+                    text={tokenPrice ? customFixed(tokenPrice, activeToken.fixedDecimals) : null}
                 />
             </Container>
         )
-    }, [selectedNetwork]);
+    }, [activeToken, selectedNetwork, w3]);
 }
 
 export default Statistics;
