ce202bd3361f86e98d3090b54ac27e8d0a076f28
yologolf <surprise.eng000@gmail.com>
2021-11-29 21:38:37 +0200

    Burn action, TODO: add request details

--- src/components/pages/Arbitrage/ArbitrageModal/Views/Burn.js
+++ src/components/pages/Arbitrage/ArbitrageModal/Views/Burn.js
@@ -1,23 +1,64 @@
-import React, { useMemo, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import Title from "components/Elements/Title";
 import Stat from "components/Stat";
 import Button from "components/Elements/Button";
 import CountdownComponent from "components/Countdown";
+import { useDispatch } from 'react-redux';
+import { appViewContext } from 'components/Context';
+import { useActiveToken } from 'components/Hooks';
+import { addAlert } from 'store/actions';
+import config from 'config/config';
+import { useActiveWeb3React } from 'components/Hooks/wallet';
 
-const Burn = ({ closeBtn }) => {
-    const activeToken = "USDC";
-    const [amount] = useState(100)
-    const [upFrontPayment] = useState(1000)
-    const [fullfillmentIn] = useState(17280000)
-    const [timeToFullfillment] = useState(6)
-    const [fee] = useState(0.3)
-    const [amountToFullfill] = useState(1700)
-    const [recieveAmount] = useState(900)
-    const [estimatedAmount] = useState(15)
+const Burn = ({ closeBtn, requestData }) => {
+  const activeToken = useActiveToken();
+  const { w3 } = useContext(appViewContext);
+  const { account } = useActiveWeb3React();
+  const [preFulfillData, setPreFulfillData] = useState(null)
+  const [amount] = useState(100)
+  const [upFrontPayment] = useState(1000)
+  const [fullfillmentIn] = useState(17280000)
+  const [timeToFullfillment] = useState(6)
+  const [fee] = useState(0.3)
+  const [amountToFullfill] = useState(1700)
+  const [recieveAmount] = useState(900)
+  const [estimatedAmount] = useState(15)
+  const dispatch = useDispatch();
+    
+  const onClick = async() => {
+    try {
+      await w3?.tokens[activeToken.rel.contractKey].fulfillBurn(requestData.requestId, account);
+      dispatch(addAlert({
+        id: 'mint',
+        eventName: "Mint - failed",
+        alertType: config.alerts.types.CONFIRMED,
+        message: "Transaction failed!"
+      }));
+    } catch {
+      dispatch(addAlert({
+        id: 'mint',
+        eventName: "Mint - failed",
+        alertType: config.alerts.types.FAILED,
+        message: "Transaction failed!"
+      }));
+    } finally {
+      closeBtn();
+    }
+  }
 
-    return useMemo(() => {
-        return (
-            <>
+  useEffect(()=>{
+    const preFulfill = async () => {
+      const preFulfillRes = await w3?.tokens[activeToken.rel.contractKey].preFulfill(requestData.requestId)
+      // const { fulfillFees, fulfillFeesPercent, receive } = preFulfillRes;
+      setPreFulfillData(preFulfillRes)
+    }
+    if(w3?.tokens[activeToken.rel.contractKey]) preFulfill();
+  },[w3, requestData, activeToken]);
+    
+  return useMemo(() => {
+    console.log("preFulfillData: ", preFulfillData);
+      return (
+          <>
             <Title
               className="arbitrage-title"
               color="white"
@@ -28,14 +69,14 @@ const Burn = ({ closeBtn }) => {
               title="Amount"
               className="bold amount"
               value={amount || "0"}
-              _suffix={activeToken}
+              _suffix={activeToken.name}
             />
       
             <Stat
               title="Up front payment"
               className="large-value bold"
               value={upFrontPayment || "0"}
-              _suffix={activeToken}
+              _suffix={activeToken.name}
             />
             
             <div className="stat-component">
@@ -66,7 +107,7 @@ const Burn = ({ closeBtn }) => {
               className="large-value bold"
               value={amountToFullfill || "0"}
               format={`${amountToFullfill || "0"}`}
-              _suffix={activeToken}
+              _suffix={activeToken.name}
             />
       
             <Stat
@@ -82,7 +123,7 @@ const Burn = ({ closeBtn }) => {
               className="large-value bold green"
               value={estimatedAmount || "0"}
               format={`${estimatedAmount || "0"}`}
-              _suffix={activeToken}
+              _suffix={activeToken.name}
             />
       
             <Button
@@ -90,6 +131,7 @@ const Burn = ({ closeBtn }) => {
               buttonText={"Fullfill"}
               processing={false}
               disabled={false}
+              onClick={onClick}
             />
 
             <Button
@@ -99,17 +141,10 @@ const Burn = ({ closeBtn }) => {
               disabled={false}
               onClick={closeBtn}
             />
-          </>
-        )
-    }, [amount,
-      upFrontPayment,
-      fullfillmentIn,
-      timeToFullfillment,
-      fee,
-      amountToFullfill,
-      recieveAmount,
-      estimatedAmount,
-      closeBtn])
+        </>
+      )
+  // eslint-disable-next-line 
+  }, [preFulfillData])
 }
 
 export default Burn
