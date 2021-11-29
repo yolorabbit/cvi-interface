d47bd1d1a40ef090653f11f72e8b52a5ba95dc55
yologolf <surprise.eng000@gmail.com>
2021-11-29 16:34:14 +0200

    useArbitrageEvents fix indent

--- src/components/Hooks/useArbitrageEvents/useArbitrageEvents.js
+++ src/components/Hooks/useArbitrageEvents/useArbitrageEvents.js
@@ -4,67 +4,65 @@ import { setData, setUnfulfilledRequests } from 'store/actions/wallet';
 import { useActiveWeb3React } from '../wallet';
 
 const useArbitrageEvents = (w3, activeToken) => {
-    const dispatch = useDispatch();
-    const [{unfulfilledRequests, arbitrage}, events] = useSelector(({wallet, events}) => [wallet, events]);
-    const { account } = useActiveWeb3React();
+  const dispatch = useDispatch();
+  const [{unfulfilledRequests, arbitrage}, events] = useSelector(({wallet, events}) => [wallet, events]);
+  const { account } = useActiveWeb3React();
 
-    useEffect(()=> { 
-        if(!activeToken?.rel || !account || !w3?.tokens) return;
+  useEffect(()=> {
+    if(!activeToken?.rel || !account || !w3?.tokens) return;
 
-        const fetchUnfulfilledRequests = async () => {
-          const unfulfilledRequests = await w3?.tokens[activeToken.rel.contractKey].getUnfulfilledRequests({account});
-          dispatch(setUnfulfilledRequests(unfulfilledRequests))
-        }
-    
-        if(!unfulfilledRequests) {
-          fetchUnfulfilledRequests();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [w3?.tokens, account, activeToken?.rel]);
-    
-      useEffect(()=>{
-        if(!activeToken?.rel || !w3?.tokens) return;
-        
-        const fetchHistory = async () => {
-          const history = await w3?.tokens[activeToken.rel.contractKey].getHistory({account});
-          dispatch(setData("arbitrage", history));
-        }
-        if(!arbitrage && account && w3?.tokens) {
-          fetchHistory();
-        }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [w3?.tokens, account, activeToken?.rel]);
-    
-      useEffect(()=>{
-        if(!events || !events[activeToken?.rel?.contractKey] || !w3?.tokens) return;
-    
-        const getUnfulfilledRequestsById = async (requestId, lastEvent) => {
-          const lastRequest = await w3?.tokens[activeToken.rel.contractKey].getRequest(requestId);
-          if(lastRequest) {
-            const { owner, raw: { tokenAmount }, requestType, targetTimestamp, requestTimestamp } = lastRequest
-            const eventData = {
-              account: owner,
-              event: "SubmitRequest",
-              requestId,
-              requestType,
-              submitFeesAmount: lastEvent.submitFeesAmount,
-              targetTimestamp,
-              timestamp: requestTimestamp,
-              tokenAmount
-            }
-    
-            dispatch(setUnfulfilledRequests(eventData, true))
-          }
-        }
-        const longTokenUnfulfilledEvents = events[activeToken.rel.contractKey]?.SubmitRequest.events;
-        if(!!longTokenUnfulfilledEvents?.length) {
-          const lastEvent = longTokenUnfulfilledEvents[longTokenUnfulfilledEvents.length-1];
-          getUnfulfilledRequestsById(lastEvent.requestId, lastEvent);
-        }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [events[activeToken?.rel?.contractKey]?.SubmitRequest.events.length]);
+    const fetchUnfulfilledRequests = async () => {
+      const unfulfilledRequests = await w3?.tokens[activeToken.rel.contractKey].getUnfulfilledRequests({account});
+      dispatch(setUnfulfilledRequests(unfulfilledRequests))
+    }
 
-    return [{unfulfilledRequests, arbitrage}, events];
+    if(!unfulfilledRequests) {
+      fetchUnfulfilledRequests();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [w3?.tokens, account, activeToken?.rel]);
+    
+  useEffect(()=>{
+    if(!activeToken?.rel || !w3?.tokens) return;
+    
+    const fetchHistory = async () => {
+      const history = await w3?.tokens[activeToken.rel.contractKey].getHistory({account});
+      dispatch(setData("arbitrage", history));
+    }
+    if(!arbitrage && account && w3?.tokens) {
+      fetchHistory();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [w3?.tokens, account, activeToken?.rel]);
+    
+  useEffect(()=>{
+    if(!events || !events[activeToken?.rel?.contractKey] || !w3?.tokens) return;
+
+    const getUnfulfilledRequestsById = async (requestId, lastEvent) => {
+      const lastRequest = await w3?.tokens[activeToken.rel.contractKey].getRequest(requestId);
+      if(lastRequest) {
+        const { owner, raw: { tokenAmount }, requestType, targetTimestamp, requestTimestamp } = lastRequest
+        const eventData = {
+          account: owner,
+          event: "SubmitRequest",
+          requestId,
+          requestType,
+          submitFeesAmount: lastEvent.submitFeesAmount,
+          targetTimestamp,
+          timestamp: requestTimestamp,
+          tokenAmount
+        }
+
+        dispatch(setUnfulfilledRequests(eventData, true))
+      }
+    }
+    const longTokenUnfulfilledEvents = events[activeToken.rel.contractKey]?.SubmitRequest.events;
+    if(!!longTokenUnfulfilledEvents?.length) {
+      const lastEvent = longTokenUnfulfilledEvents[longTokenUnfulfilledEvents.length-1];
+      getUnfulfilledRequestsById(lastEvent.requestId, lastEvent);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [events[activeToken?.rel?.contractKey]?.SubmitRequest.events.length]);
 }
 
 export default useArbitrageEvents;
