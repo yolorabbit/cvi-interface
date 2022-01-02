6fe491336a8fb7f9dabcf26d7f08f3a917dc5420
yologolf <surprise.eng000@gmail.com>
2022-01-02 15:30:11 +0200

    fix statistics get prices

--- src/components/pages/Arbitrage/Statistics/Statistics.js
+++ src/components/pages/Arbitrage/Statistics/Statistics.js
@@ -4,7 +4,7 @@ import Container from 'components/Layout/Container';
 import IndexValue from 'components/pages/Platform/IndexStats/IndexValue';
 import { Value } from 'components/Tables/Elements/Values';
 import arbitrageConfig from 'config/arbitrageConfig';
-import React, { useContext, useMemo } from 'react'
+import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { customFixed } from 'utils';
 import './Statistics.scss';
@@ -15,16 +15,43 @@ const Statistics = () => {
     const activeToken = useActiveToken();
     const isTablet = useIsTablet();
     const isMobile = useIsMobile();
+    const [statisticsDetails, setStatisticsDetails] = useState({
+        tokenPrice: null,
+        intrinsicValue: null
+    });
 
+    const getStatisticsDetails = useCallback(async (tokenContract) => {
+        try {
+            const [tokenPrice, intrinsicValue] = [await tokenContract.getUSDPrice(), await tokenContract.getIntrinsicPrice()];
+            setStatisticsDetails({
+                tokenPrice,
+                intrinsicValue
+            });
+        } catch(error) {
+            console.log(error);
+            setStatisticsDetails({
+                tokenPrice: "N/A",
+                intrinsicValue: "N/A"
+            })
+        }
+    }, [])
+
+    useEffect(() => {
+        const tokenContract = w3?.tokens?.[activeToken?.rel?.volTokenKey];
+        if(!tokenContract) return;
+
+        const { tokenPrice, intrinsicValue} = statisticsDetails;
+        if(!tokenPrice && !intrinsicValue) {
+            getStatisticsDetails(tokenContract)
+        }
+    }, [activeToken?.rel?.volTokenKey, getStatisticsDetails, statisticsDetails, w3?.tokens]);
+
+    
     return useMemo(() => {
+        
         if (!selectedNetwork || !activeToken) return null;
         const { pairToken } = activeToken;
         const loadActiveVol = Object.values(arbitrageConfig.tokens[selectedNetwork]).map(({ oracleId }) => oracleId)[0];
-        const tokenContract = w3?.tokens[activeToken.rel.volTokenKey];
-        const statisticsDetails = {
-            tokenPrice: tokenContract?.uniswapPrice ?? null,
-            intrinsicValue: tokenContract?.intrinsicPrice ?? null,
-        }
         const { tokenPrice, intrinsicValue } = statisticsDetails;
 
         return (
@@ -56,7 +83,7 @@ const Statistics = () => {
                 </a>
             </Container>
         )
-    }, [activeToken, selectedNetwork, w3, isTablet, isMobile]);
+    }, [selectedNetwork, activeToken, statisticsDetails, isMobile, isTablet]);
 }
 
 export default Statistics;
