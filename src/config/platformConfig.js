7cc4d469f74d06ba4ab4169789bb8703589dd6dd
yologolf <surprise.eng000@gmail.com>
2022-02-23 08:30:28 +0200

    Removed USDT tokens config from Ethereum and Polygon networks (#2)

--- src/config/platformConfig.js
+++ src/config/platformConfig.js
@@ -70,28 +70,6 @@ const platformConfig = {
           liquidation: "CVOL-USDT-Liquidation" // there is only one liquidation contract for both eth and usdt.
         },
       },
-      usdt: {
-        migrated: config.isMainnet,
-        key: "usdt",
-        name: "usdt",
-        decimals: 6,
-        fixedDecimals: 6,
-        lpTokensDecimals: 18,
-        type: "v1",
-        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-        oracleId: config.oraclesData.cvi.oracleId,
-        rel: {
-          contractKey: "USDT",
-          platform: "CVOL-USDT-Platform",
-          feesCalc: "CVOL-USDT-FeesCalculator",
-          feesModel: "CVOL-USDT-FeesModel",
-          oracle: "CVOL-Oracle-ETH", // same oracle for eth and usdt
-          positionRewards: "CVOL-USDT-PositionRewards",
-          stakingRewards: "CVOL-USDTLP-StakingRewards",
-          liquidation: "CVOL-USDT-Liquidation",
-          rewards: "Rewards",
-        },
-      },
       "usdc-ethvi": {
         key: "usdc-ethvi",
         name: "usdc",
@@ -142,27 +120,6 @@ const platformConfig = {
           stakingRewards: "CVOL-USDCLP-StakingRewards",
           liquidation: "CVOL-USDC-Liquidation"
         }
-      },
-      usdt: {
-        migrated: config.isMainnet,
-        key: "usdt",
-        name: "usdt",
-        decimals: 6,
-        fixedDecimals: 6,
-        lpTokensDecimals: 18,
-        type: "v2",
-        address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
-        oracleId: config.oraclesData.cvi.oracleId,
-        rel: {
-          contractKey: "USDT",
-          platform: "CVOL-USDT-Platform",
-          feesCalc: "CVOL-USDT-FeesCalculator",
-          feesModel: "CVOL-USDT-Platform",
-          positionRewards: "CVOL-USDT-PositionRewards",
-          oracle: "CVOL-Oracle",
-          stakingRewards: "CVOL-USDTLP-StakingRewards",
-          liquidation: "CVOL-USDT-Liquidation"
-        },
       },
     },
     [chainNames.Arbitrum]: {
